46f10b04c8572af611a3a82c384d55d52ed947ae
Hakimi Mdnoor <74492300+tomyamGTX@users.noreply.github.com>
2021-12-15 19:42:54 +0800

    Delete lib directory

--- lib/Services/Firebase.quiz.dart
+++
@@ -1,4 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-final CollectionReference comicCollection =
-FirebaseFirestore.instance.collection('quiz');
--- lib/main/utils/ListModels.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ListModel {
-  String? name;
-  Widget? widget;
-
-  ListModel({this.name, this.widget});
-}
